b7da1b172bf1f411392c54e4eb95194e5b6253f9
ZackHine <zackhine@gmail.com>
2015-07-04 00:13:26 -0400

    Make GenerateView Task support view-element and namespaces properties from view-element-build.json fully.

--- src/tasks/generateViews/GenerateView.php
+++ src/tasks/generateViews/GenerateView.php
@@ -13,6 +13,11 @@ include_once "PropertiesTemplate.php";
 include_once "ViewElementsArrayTemplate.php";
 
 class GenerateView extends \Task {
+    const VIEW_ELEMENT_KEY = "view-element";
+    const DIR_SEP_KEY = "dir-sep";
+    const NAMESPACES_KEY = "namespaces";
+    const DEFAULT_VIEW_ELEMENT_VALUE = "ViewElement";
+    const DEFAULT_DIR_SEP_VALUE = "\\";
 
     /**
      * The name passed in the buildfile.
@@ -33,99 +38,122 @@ class GenerateView extends \Task {
         // nothing to do here
     }
 
+
+
     /**
      * The main entry point method.
      */
     public function main() {
-        $matches = array();
-        // TODO: This path needs to come from a config file or something
-        preg_match('@^(.*\\\\)*(.*)\.html@i', $this->name, $matches);
+        $fileNameMatches = array();
 
-        // TODO: namespace needs to come from a view-element-build.json file:
-        // TODO:
-        /**
-         * namespaces: [
-         *      "src": "namespace"
-         * ]
-         */
-        // TODO: Then we look for it based on $this->name. If doesn't exist, no namespace
+        $this->setUpFileNameMatches($fileNameMatches);
 
-        $viewElementBuild = file_get_contents("view-element-build.json");// TODO: What if doesn't exist?
-        $viewElementBuild = json_decode($viewElementBuild, true);
+        try {
+            $viewElementBuild = file_get_contents("view-element-build.json");
+            $viewElementBuild = json_decode($viewElementBuild, true);
+
+            $viewElementValue = $this->setUpViewElement($viewElementBuild);
+            $namespaceValue = $this->setUpNamespace($viewElementBuild, $fileNameMatches);
+
+            $viewFile = file_get_contents($this->name);
+            preg_match_all('@<.* id\s*=\s*["|\']{{'.$viewElementValue.'_(.*)}}["|\'].*>@i', $viewFile, $viewMatches);// find all elements in html with id starting with $viewElementValue
+
+            $template = getViewTemplate();// bring in basic template for a class that implements IView
+
+            $vars = array();
+
+            if($namespaceValue !== null) {
+                $vars["namespace"] = "namespace ".$namespaceValue.";";
+            } else {
+                $vars["namespace"] = "";
+            }
+            $vars["class_name"] = ucfirst($fileNameMatches[2]);
+            $vars["view_file_name"] = $this->name;
+            $template = $this->replaceTokens($vars, $template);
+
+            $propertiesPiece = "";
+            $constructPiece = "";
+            $viewElementsArrayPiece = "";
+            $cnt = 1;
+
+            foreach($viewMatches[1] as $viewElement) {
+                $dom = new DOMDocument();
+                $dom->loadHTML($viewFile);
+                echo "DOM HTML:";
+
+                $veVars["property_name"] = $viewElement;
+                $constructTemplate = getConstructViewElementsTemplate();
+
+                // constructor
+                $vars = array();
+                $vars["property_name"] = $viewElement;
+
+                $propertiesTemplate = getPropertiesTemplate();
+                $propertiesPiece .=  $this->replaceTokens($vars, $propertiesTemplate);
+
+                $constructPiece .=  $this->replaceTokens($veVars, $constructTemplate);
+
+                // view elements array
+                $vars = array();
+                $vars["property_name"] = $viewElement;
+                $vars["comma"] = count($viewMatches[1]) === $cnt ? "" : ",";
+
+                $viewElementsArrayTemplate = getViewElementsArrayTemplate();
+                $viewElementsArrayPiece .=  $this->replaceTokens($vars, $viewElementsArrayTemplate);
+
+                ++$cnt;
+            }
+            $template = $this->replaceToken("view_element_declarations", $propertiesPiece, $template);
+            $template = $this->replaceToken("constructor", $constructPiece, $template);
+            $template = $this->replaceToken("view_elements_array", $viewElementsArrayPiece, $template);
+
+            echo $template;
+
+            $fh = fopen($fileNameMatches[1].ucfirst($fileNameMatches[2]).".php", "w");// TODO: Need to get those backslashes turned around?
+            fwrite($fh, $template);
+            fclose($fh);
+        }
+        catch (Exception $e) {
+            echo "Error: view-element-build.json must exist at root";
+        }
+    }
+
+    /**
+     * Take an empty array and populate it with path to current file not including
+     * the file, and the file name
+     * @param array $fileNameMatches
+     */
+    protected function setUpFileNameMatches(array &$fileNameMatches) {
+        preg_match('@^(.*\\\\)*(.*)\.html@i', $this->name, $fileNameMatches);
+    }
+
+    /**
+     * Given associative array representation of view-element-build.json, and fileNameMatches for current file,
+     * returns namespace in view-element-build or null if not defined
+     * @param $viewElementBuild
+     * @param $fileNameMatches
+     * @return null|string
+     */
+    protected function setUpNamespace($viewElementBuild, $fileNameMatches) {
         $namespace = null;
-        if($viewElementBuild["namespaces"] !== null) {
-            foreach($viewElementBuild["namespaces"] as $namespace) {
-                if($namespace[$matches[1]] !== null) {
-                    $namespace = $namespace[$matches[1]];
+        if($viewElementBuild[GenerateView::NAMESPACES_KEY] !== null) {
+            foreach($viewElementBuild[GenerateView::NAMESPACES_KEY] as $namespace) {
+                if($namespace[$fileNameMatches[1]] !== null) {
+                    $namespace = $namespace[$fileNameMatches[1]];
                     break;
                 }
             }
         }
+        return $namespace;
+    }
 
-        $viewFile = file_get_contents($this->name);
-        // TODO: ALViewElement should be configurable or at least be just "ViewElement"
-        preg_match_all('@<.* id\s*=\s*["|\']{{ALViewElement_(.*)}}["|\'].*>@i', $viewFile, $viewMatches);
-
-        $template = getViewTemplate();
-
-        $vars = array();
-        // TODO: We shouldn't need a command name, we need to figure out another way to do it
-        $vars["command_name"] = ucfirst($matches[1]);
-        if($vars["command_name"] == "Common") {
-            $vars["command_name"] = "common";
-        }
-        if($namespace !== null) {
-            $vars["namespace"] = "namespace ".$namespace.";";
-        } else {
-            $vars["namespace"] = "";
-        }
-        $vars["class_name"] = ucfirst($matches[2]);
-        $vars["view_file_name"] = $this->name;
-        $template = $this->replaceTokens($vars, $template);
-
-        $propertiesPiece = "";
-        $constructPiece = "";
-        $viewElementsArrayPiece = "";
-        $cnt = 1;
-
-        echo "-->";print_r($viewMatches);
-        foreach($viewMatches[1] as $viewElement) {
-            $dom = new DOMDocument();
-            $dom->loadHTML($viewFile);
-            echo "DOM HTML:";
-
-            $veVars["property_name"] = $viewElement;
-            $constructTemplate = getConstructViewElementsTemplate();
-
-            // constructor
-            $vars = array();
-            $vars["property_name"] = $viewElement;
-
-            $propertiesTemplate = getPropertiesTemplate();
-            $propertiesPiece .=  $this->replaceTokens($vars, $propertiesTemplate);
-
-            $constructPiece .=  $this->replaceTokens($veVars, $constructTemplate);
-
-            // view elements array
-            $vars = array();
-            $vars["property_name"] = $viewElement;
-            $vars["comma"] = count($viewMatches[1]) === $cnt ? "" : ",";
-
-            $viewElementsArrayTemplate = getViewElementsArrayTemplate();
-            $viewElementsArrayPiece .=  $this->replaceTokens($vars, $viewElementsArrayTemplate);
-
-            ++$cnt;
-        }
-        $template = $this->replaceToken("view_element_declarations", $propertiesPiece, $template);
-        $template = $this->replaceToken("constructor", $constructPiece, $template);
-        $template = $this->replaceToken("view_elements_array", $viewElementsArrayPiece, $template);
-
-        echo $template;
-
-        // TODO: Again, the path to our views needs to be configurable somehow
-        $fh = fopen($matches[1].ucfirst($matches[2]).".php", "w");// TODO: Need to get those backslashes turned around?
-        fwrite($fh, $template);
-        fclose($fh);
+    /**
+     * Given associative array representation of view-element-build.json, returns defined view-element value or "ViewElement" by default
+     * @param $viewElementBuild
+     * @return string
+     */
+    protected function setUpViewElement($viewElementBuild) {
+        return $viewElementBuild[GenerateView::VIEW_ELEMENT_KEY] !== null ? $viewElementBuild[GenerateView::VIEW_ELEMENT_KEY] : GenerateView::DEFAULT_VIEW_ELEMENT_VALUE;
     }
 
     private function replaceTokens($tokens, $template) {
